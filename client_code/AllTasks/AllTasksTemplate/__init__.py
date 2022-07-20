57b3dff349c72c6fb158f13eb81ada267e329abe
agocoded@gmail.com <agocoded@gmail.com>
2022-07-20 14:54:02 +0000

    Edited TaskEdit and AllTasks.AllTasksTemplate

--- client_code/AllTasks/AllTasksTemplate/__init__.py
+++ client_code/AllTasks/AllTasksTemplate/__init__.py
@@ -3,6 +3,7 @@ from anvil import *
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+from ...TaskEdit import TaskEdit
 
 class AllTasksTemplate(AllTasksTemplateTemplate):
   def __init__(self, **properties):
@@ -10,3 +11,16 @@ class AllTasksTemplate(AllTasksTemplateTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
+
+  def link_edit_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    alert(TaskEdit(item = self.item), title="Edit Your Task Details", large=True)
+    
+    self.refresh_data_bindings()
+    pass
+
+  def check_box_1_change(self, **event_args):
+    """This method is called when this checkbox is checked or unchecked"""
+    pass
+
+
