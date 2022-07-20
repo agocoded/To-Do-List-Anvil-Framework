a082f7cd6583b02b4f71b2f375ce446c233a7f76
agocoded@gmail.com <agocoded@gmail.com>
2022-07-20 11:23:03 +0000

    Edited AllTasks and AllTasks.ItemTemplate1

--- client_code/AllTasks/__init__.py
+++ client_code/AllTasks/__init__.py
@@ -10,4 +10,4 @@ class AllTasks(AllTasksTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
-    
+    self.repeating_panel_1.items = app_tables.todos.search()
