65f7c2c2d2c4858c4164eae272e7488bc885fd33
agocoded@gmail.com <agocoded@gmail.com>
2022-07-23 23:19:18 +0000

    added ability to create, read, update and delete

--- client_code/AllTasks/AllTasksTemplate/__init__.py
+++ client_code/AllTasks/AllTasksTemplate/__init__.py
@@ -16,27 +16,30 @@ class AllTasksTemplate(AllTasksTemplateTemplate):
   def link_edit_click(self, **event_args):
     """This method is called when the link is clicked"""
     alert(TaskEdit(item = self.item), title="Edit Your Task Details", large=True)
-    
-    self.refresh_data_bindings()
-    pass      
+    self.refresh_data_bindings()   
 
   def check_box_1_change(self, **event_args):
     """This method is called when this checkbox is checked or unchecked"""
     if (self.item['done']) == True:
       self.item['completed on'] = datetime.now()
-    
-    pass
+
 
   def link_1_click(self, **event_args):
     """This method is called when the link is clicked"""
     description = str(self.item['description'])
-    current_time = self.item['completed on'].strftime('%d %b %Y %I:%M%p')
     
-    if self.item['done'] == True:
+    if self.item['completed on']:
+      current_time = self.item['completed on'].strftime('%d %b %Y %I:%M%p')
       alert(description + '\n'+ 'Task Completed on: ' + current_time)
     else:
       alert(description)
-    pass
+
+  def delete_link_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.item.delete()
+    self.remove_from_parent()
+    #self.refresh_data_bindings()
+
 
 
 
